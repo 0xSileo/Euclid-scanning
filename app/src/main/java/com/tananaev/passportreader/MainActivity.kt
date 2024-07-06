86b045d1bb7f5ea31421b35d530b88bc4a3f5822
0xSileo <github@sileo.dev>
2024-07-06 19:03:59 +0200

    Removed code unnecessary to SOD data reading
    Signed-off-by: 0xSileo <github@sileo.dev>

--- app/src/main/java/com/tananaev/passportreader/MainActivity.kt
+++ app/src/main/java/com/tananaev/passportreader/MainActivity.kt
@@ -36,33 +36,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
 import net.sf.scuba.smartcards.CardService
-import org.apache.commons.io.IOUtils
-import org.bouncycastle.asn1.ASN1InputStream
-import org.bouncycastle.asn1.ASN1Primitive
-import org.bouncycastle.asn1.ASN1Sequence
-import org.bouncycastle.asn1.ASN1Set
-import org.bouncycastle.asn1.x509.Certificate
 import org.jmrtd.BACKey
 import org.jmrtd.BACKeySpec
 import org.jmrtd.PassportService
 import org.jmrtd.lds.CardAccessFile
-import org.jmrtd.lds.ChipAuthenticationPublicKeyInfo
 import org.jmrtd.lds.PACEInfo
 import org.jmrtd.lds.SODFile
 import org.jmrtd.lds.SecurityInfo
-import org.jmrtd.lds.icao.DG14File
-import org.jmrtd.lds.icao.DG1File
-import org.jmrtd.lds.icao.DG2File
-import java.io.ByteArrayInputStream
-import java.security.KeyStore
-import java.security.MessageDigest
-import java.security.Signature
-import java.security.cert.CertPathValidator
-import java.security.cert.CertificateFactory
-import java.security.cert.PKIXParameters
-import java.security.cert.X509Certificate
-import java.security.spec.MGF1ParameterSpec
-import java.security.spec.PSSParameterSpec
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -199,13 +179,7 @@ abstract class MainActivity : AppCompatActivity() {
     @SuppressLint("StaticFieldLeak")
     private inner class ReadTask(private val isoDep: IsoDep, private val bacKey: BACKeySpec) : AsyncTask<Void?, Void?, Exception?>() {
 
-        private lateinit var dg1File: DG1File
-        private lateinit var dg2File: DG2File
-        private lateinit var dg14File: DG14File
         private lateinit var sodFile: SODFile
-        private var chipAuthSucceeded = false
-        private var passiveAuthSuccess = false
-        private lateinit var dg14Encoded: ByteArray
 
         override fun doInBackground(vararg params: Void?): Exception? {
             try {
@@ -246,106 +220,14 @@ abstract class MainActivity : AppCompatActivity() {
                         service.doBAC(bacKey)
                     }
                 }
-                val dg1In = service.getInputStream(PassportService.EF_DG1)
-                dg1File = DG1File(dg1In)
-                val dg2In = service.getInputStream(PassportService.EF_DG2)
-                dg2File = DG2File(dg2In)
                 val sodIn = service.getInputStream(PassportService.EF_SOD)
                 sodFile = SODFile(sodIn)
 
-                doChipAuth(service)
-                doPassiveAuth()
 
             } catch (e: Exception) {
                 return e
             }
             return null
-        }
-
-        private fun doChipAuth(service: PassportService) {
-            try {
-                val dg14In = service.getInputStream(PassportService.EF_DG14)
-                dg14Encoded = IOUtils.toByteArray(dg14In)
-                val dg14InByte = ByteArrayInputStream(dg14Encoded)
-                dg14File = DG14File(dg14InByte)
-                val dg14FileSecurityInfo = dg14File.securityInfos
-                for (securityInfo: SecurityInfo in dg14FileSecurityInfo) {
-                    if (securityInfo is ChipAuthenticationPublicKeyInfo) {
-                        service.doEACCA(
-                            securityInfo.keyId,
-                            ChipAuthenticationPublicKeyInfo.ID_CA_ECDH_AES_CBC_CMAC_256,
-                            securityInfo.objectIdentifier,
-                            securityInfo.subjectPublicKey,
-                        )
-                        chipAuthSucceeded = true
-                    }
-                }
-            } catch (e: Exception) {
-                Log.w(TAG, e)
-            }
-        }
-
-        private fun doPassiveAuth() {
-            try {
-                val digest = MessageDigest.getInstance(sodFile.digestAlgorithm)
-                val dataHashes = sodFile.dataGroupHashes
-                val dg14Hash = if (chipAuthSucceeded) digest.digest(dg14Encoded) else ByteArray(0)
-                val dg1Hash = digest.digest(dg1File.encoded)
-                val dg2Hash = digest.digest(dg2File.encoded)
-
-                if (Arrays.equals(dg1Hash, dataHashes[1]) && Arrays.equals(dg2Hash, dataHashes[2])
-                    && (!chipAuthSucceeded || Arrays.equals(dg14Hash, dataHashes[14]))) {
-
-                    val asn1InputStream = ASN1InputStream(assets.open("masterList"))
-                    val keystore = KeyStore.getInstance(KeyStore.getDefaultType())
-                    keystore.load(null, null)
-                    val cf = CertificateFactory.getInstance("X.509")
-
-                    var p: ASN1Primitive?
-                    while (asn1InputStream.readObject().also { p = it } != null) {
-                        val asn1 = ASN1Sequence.getInstance(p)
-                        if (asn1 == null || asn1.size() == 0) {
-                            throw IllegalArgumentException("Null or empty sequence passed.")
-                        }
-                        if (asn1.size() != 2) {
-                            throw IllegalArgumentException("Incorrect sequence size: " + asn1.size())
-                        }
-                        val certSet = ASN1Set.getInstance(asn1.getObjectAt(1))
-                        for (i in 0 until certSet.size()) {
-                            val certificate = Certificate.getInstance(certSet.getObjectAt(i))
-                            val pemCertificate = certificate.encoded
-                            val javaCertificate = cf.generateCertificate(ByteArrayInputStream(pemCertificate))
-                            keystore.setCertificateEntry(i.toString(), javaCertificate)
-                        }
-                    }
-
-                    val docSigningCertificates = sodFile.docSigningCertificates
-                    for (docSigningCertificate: X509Certificate in docSigningCertificates) {
-                        docSigningCertificate.checkValidity()
-                    }
-
-                    val cp = cf.generateCertPath(docSigningCertificates)
-                    val pkixParameters = PKIXParameters(keystore)
-                    pkixParameters.isRevocationEnabled = false
-                    val cpv = CertPathValidator.getInstance(CertPathValidator.getDefaultType())
-                    cpv.validate(cp, pkixParameters)
-                    var sodDigestEncryptionAlgorithm = sodFile.docSigningCertificate.sigAlgName
-                    var isSSA = false
-                    if ((sodDigestEncryptionAlgorithm == "SSAwithRSA/PSS")) {
-                        sodDigestEncryptionAlgorithm = "SHA256withRSA/PSS"
-                        isSSA = true
-                    }
-                    val sign = Signature.getInstance(sodDigestEncryptionAlgorithm)
-                    if (isSSA) {
-                        sign.setParameter(PSSParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, 32, 1))
-                    }
-                    sign.initVerify(sodFile.docSigningCertificate)
-                    sign.update(sodFile.eContent)
-                    passiveAuthSuccess = sign.verify(sodFile.encryptedDigest)
-                }
-            } catch (e: Exception) {
-                Log.w(TAG, e)
-            }
         }
 
         override fun onPostExecute(result: Exception?) {
@@ -357,24 +239,6 @@ abstract class MainActivity : AppCompatActivity() {
                 } else {
                     Intent(this@MainActivity, ResultActivity::class.java)
                 }
-                val mrzInfo = dg1File.mrzInfo
-                intent.putExtra(ResultActivity.KEY_FIRST_NAME, mrzInfo.secondaryIdentifier.replace("<", " "))
-                intent.putExtra(ResultActivity.KEY_LAST_NAME, mrzInfo.primaryIdentifier.replace("<", " "))
-                intent.putExtra(ResultActivity.KEY_GENDER, mrzInfo.gender.toString())
-                intent.putExtra(ResultActivity.KEY_STATE, mrzInfo.issuingState)
-                intent.putExtra(ResultActivity.KEY_NATIONALITY, mrzInfo.nationality)
-                val passiveAuthStr = if (passiveAuthSuccess) {
-                    getString(R.string.pass)
-                } else {
-                    getString(R.string.failed)
-                }
-                val chipAuthStr = if (chipAuthSucceeded) {
-                    getString(R.string.pass)
-                } else {
-                    getString(R.string.failed)
-                }
-                intent.putExtra(ResultActivity.KEY_PASSIVE_AUTH, passiveAuthStr)
-                intent.putExtra(ResultActivity.KEY_CHIP_AUTH, chipAuthStr)
 
                 if (callingActivity != null) {
                     setResult(RESULT_OK, intent)
--- app/src/main/java/com/tananaev/passportreader/ResultActivity.kt
+++ app/src/main/java/com/tananaev/passportreader/ResultActivity.kt
@@ -16,7 +16,6 @@
 package com.tananaev.passportreader
 
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -24,22 +23,8 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        findViewById<TextView>(R.id.output_first_name).text = intent.getStringExtra(KEY_FIRST_NAME)
-        findViewById<TextView>(R.id.output_last_name).text = intent.getStringExtra(KEY_LAST_NAME)
-        findViewById<TextView>(R.id.output_gender).text = intent.getStringExtra(KEY_GENDER)
-        findViewById<TextView>(R.id.output_state).text = intent.getStringExtra(KEY_STATE)
-        findViewById<TextView>(R.id.output_nationality).text = intent.getStringExtra(KEY_NATIONALITY)
-        findViewById<TextView>(R.id.output_passive_auth).text = intent.getStringExtra(KEY_PASSIVE_AUTH)
-        findViewById<TextView>(R.id.output_chip_auth).text = intent.getStringExtra(KEY_CHIP_AUTH)
     }
 
     companion object {
-        const val KEY_FIRST_NAME = "firstName"
-        const val KEY_LAST_NAME = "lastName"
-        const val KEY_GENDER = "gender"
-        const val KEY_STATE = "state"
-        const val KEY_NATIONALITY = "nationality"
-        const val KEY_PASSIVE_AUTH = "passiveAuth"
-        const val KEY_CHIP_AUTH = "chipAuth"
     }
 }
